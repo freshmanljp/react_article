74a9b2ad935154603d838b1050c98a85c9bb1eff
ljp <1498725065@qq.com>
2020-06-15 17:26:41 +0800

    配置antd以及less的自定义主题

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,16 @@
-const { override, addLessLoader } = require('customize-cra')
+const { override, addLessLoader, fixBabelImports } = require('customize-cra')
+const theme = require('./modifyVars')
 
 module.exports = override(
-  addLessLoader()
+  addLessLoader({
+    lessOptions: {
+      javascriptEnabled: true,
+      modifyVars: theme
+    },
+  }),
+  fixBabelImports('import', {
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: true
+  })
 )
